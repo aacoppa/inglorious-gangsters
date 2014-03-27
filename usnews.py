f6006aa70b5cc2cb1c97ea37cc2ae63f3a0f1334
Timothy Ong <tim.ong.118@gmail.com>
2014-03-27 19:36:12 -0400

    usnews

---
+++ usnews.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+for x in range(24):
+    url = "http://colleges.usnews.rankingsandreviews.com/best-colleges/rankings/national-universities/page+%d"%(x+1)
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data)
+    name = soup.findAll("a", {"class":"school-name"})
+    ranking = soup.findAll("span", {"class":"rankscore cluetip cluetip-stylized"})
+    for y in range(11):
+        try:
+            #print ranking[y]
+            print name[y].string
+        except:
+            pass
