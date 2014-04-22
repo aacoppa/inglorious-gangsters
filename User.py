d1a5350cee3e4a720b926e7e74172ff8b7de0062
Aaron Coppa <aacoppa@gmail.com>
2014-04-21 20:56:00 -0400

    Started compare values and difficulties

--- User.py
+++ User.py
@@ -5,7 +5,10 @@ class User():
         self.locations = []
         self.grades = {}
         self.sats = {}
+        self.schools = []
     def add_location(self, place):
         self.locations.append(place)
-    def add_grades():
+    def add_grades(grades):
+        for key in grades:
+            self.grades[key] = grades[key]
     
