8dad432436583451a9d854a35554bbf8c6deab1d
Timothy Ong <tim.ong.118@gmail.com>
2014-04-27 14:39:58 -0400

    asdf

--- initcoldb.py
+++ initcoldb.py
@@ -13,7 +13,7 @@ def db_store_college(College):
     db.colleges.insert({'name':College.name, 'location':College.location,
                         'rank':College.rank, 'sats':College.sats,
                         'size':College.size, 'tuition':College.tuition,
-                        'address':College.address})
+                        'address':College.address, 'state':College.state})
 
 def db_college_exits(College_name):
     College = db.colleges.find({'name':College_name})
