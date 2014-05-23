62071a00728d1c59901eb4d6127b11f6512aa514
Timothy Ong <tim.ong.118@gmail.com>
2014-05-23 07:14:01 -0400

    weather

--- weather.py
+++ weather.py
@@ -1,12 +1,11 @@
 from bs4 import BeautifulSoup
-import requests
+import requests,urllib2,json
 
 def almanac(zip):
     url = "http://api.wunderground.com/api/4997e70515d4cbbd/almanac/q/%d.json"%(zip)
-    r = requests.get(url)
-    data = r.text
-    soup = BeautifulSoup(data)
-    print soup.findAll("almanac")
+    r = urllib2.urlopen(url)
+    data = json.loads(r.read())
+    print data['almanac']
 
 
 almanac(11214)
