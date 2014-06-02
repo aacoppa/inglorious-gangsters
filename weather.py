9d8016a70f2a8441bc340a08ad49b084dd0d21f8
Timothy Ong <tim.ong.118@gmail.com>
2014-06-02 11:58:11 -0400

    dictionary for weather

--- weather.py
+++ weather.py
@@ -1,11 +1,16 @@
-from bs4 import BeautifulSoup
-import requests,urllib2,json
+
+import urllib2,json
 
 def almanac(zip):
     url = "http://api.wunderground.com/api/4997e70515d4cbbd/almanac/q/%d.json"%(zip)
     r = urllib2.urlopen(url)
     data = json.loads(r.read())
-    print data['almanac']
+    almanac = {"record low":data['almanac']['temp_low']['record']['F'].encode("ascii"),
+               "record high":data['almanac']['temp_high']['record']['F'].encode("ascii"),
+               "normal low":data['almanac']['temp_low']['normal']['F'].encode("ascii"),
+               "normal high":data['almanac']['temp_high']['normal']['F'].encode("ascii")
+               }
+    return almanac
 
 
-almanac(11214)
+print(almanac(11214))
