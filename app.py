345165af21aed3142ef6b77b9548b94422a83adc
Timothy Ong <tim.ong.118@gmail.com>
2014-06-12 12:05:40 -0400

    typo

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import session,request,render_template, url_for, redirect
 #from Populator import populate_database
 #import search
 #from Populator import populate_database
+from userdb import *
 
 app = Flask(__name__)
 app.secret_key="kq345bz2"
